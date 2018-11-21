f9cbd0e8adffe30c4834d97b87f70bdcaeec7b7a
Alejandra Angulo Rico <alangulo@microsoft.com>
2018-11-20 23:55:36 -0600

    Initial Commit

---
+++ testIoTAlangulo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace testIoTAlangulo
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString= System.Configuration.ConfigurationManager.AppSettings["connectionString"];
+    
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            RegisterDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task RegisterDeviceAsync()
+        {
+            string deviceId = "medicaldevice-patient1";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+
+            catch (DeviceAlreadyExistsException)
+            {
+                device= await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+
+            Console.WriteLine("Device shared access key:" + device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
